2c9785c7754f21c45e9402114800b2c267c314dd
Micah Silverman <micah@afitnerd.com>
2015-07-21 16:42:24 -0400

    Basic Spring Boot app

---
+++ src/main/java/com/stormpath/idsite_demo/IDSiteDemoApplication.java
@@ -0,0 +1,12 @@
+package com.stormpath.idsite_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDSiteDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IDSiteDemoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/stormpath/idsite_demo/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.stormpath.idsite_demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+}
