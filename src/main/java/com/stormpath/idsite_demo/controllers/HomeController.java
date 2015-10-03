28cf33d51e223fb17a1a87169866c37be5ee90f3
Micah Silverman <micah@afitnerd.com>
2015-10-03 19:04:01 -0400

    Refactored to use existing Spring Boot hooks

--- src/main/java/com/stormpath/idsite_demo/controllers/HomeController.java
+++ src/main/java/com/stormpath/idsite_demo/controllers/HomeController.java
@@ -3,7 +3,6 @@ package com.stormpath.idsite_demo.controllers;
 import com.stormpath.sdk.application.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,7 +11,7 @@ public class HomeController {
     Application app;
 
     @RequestMapping("/")
-    public String home(Model model) {
+    public String home() {
         return "index";
     }
 }
--- src/main/java/com/stormpath/idsite_demo/controllers/RestrictedController.java
+++ src/main/java/com/stormpath/idsite_demo/controllers/RestrictedController.java
@@ -3,7 +3,6 @@ package com.stormpath.idsite_demo.controllers;
 import com.stormpath.sdk.account.Account;
 import com.stormpath.sdk.application.Application;
 import com.stormpath.sdk.idsite.AccountResult;
-import com.stormpath.sdk.idsite.IdSiteUrlBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -11,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class RestrictedController {
@@ -29,56 +27,16 @@ public class RestrictedController {
 
     private static final String ID_SITE_CALLBACK = "/restricted/id_site_callback";
 
-    private String getBaseURL(HttpServletRequest request) {
-        String url = request.getRequestURL().toString();
-        if (!url.contains("localhost")) {
-            url = "https" + url.substring(url.indexOf(':'));
-        }
-        String uri = request.getRequestURI();
-        return url.substring(0, url.length() - uri.length());
-    }
+    @RequestMapping("/restricted/user")
+    public String idSiteCallback(HttpServletRequest req, Model model) {
+        AccountResult accountResult = app.newIdSiteCallbackHandler(req).getAccountResult();
+        if (accountResult != null && accountResult.getAccount() != null) {
+            Account account = accountResult.getAccount();
+            model.addAttribute("account", account);
 
-    @RequestMapping("/restricted/secret")
-    public void idSiteStep1(HttpServletRequest request, HttpServletResponse response) {
-        IdSiteUrlBuilder idSiteBuilder = app.newIdSiteUrlBuilder()
-            .setCallbackUri(getBaseURL(request) + ID_SITE_CALLBACK);
-
-        if (organization != null) {
-            idSiteBuilder
-                .setOrganizationNameKey(organization)
-                .setShowOrganizationField(showOrganizationField);
+            return "restricted/user";
         }
 
-        // This will be used by id-site to deliver the proper site content
-        idSiteBuilder.setState("site:" + templateRoot);
-
-        response.setStatus(HttpServletResponse.SC_FOUND);
-        response.setHeader("Cache-control", "no-cache, no-store");
-        response.setHeader("Pragma", "no-cache");
-        response.setHeader("Expires", "-1");
-        response.setHeader("Location", idSiteBuilder.build());
-    }
-
-    @RequestMapping(ID_SITE_CALLBACK)
-    public String idSiteStep2(HttpServletRequest request, Model model) {
-        AccountResult accountResult = app.newIdSiteCallbackHandler(request).getAccountResult();
-        Account account = accountResult.getAccount();
-
-        model.addAttribute("account", account);
-
-        return "restricted/authenticated";
-    }
-
-    @RequestMapping("/logout")
-    public void logout(HttpServletRequest request, HttpServletResponse response) {
-        IdSiteUrlBuilder idSiteBuilder = app.newIdSiteUrlBuilder();
-        idSiteBuilder.setCallbackUri(getBaseURL(request) + "/");
-        idSiteBuilder.forLogout();
-
-        response.setStatus(HttpServletResponse.SC_FOUND);
-        response.setHeader("Cache-control", "no-cache, no-store");
-        response.setHeader("Pragma", "no-cache");
-        response.setHeader("Expires", "-1");
-        response.setHeader("Location", idSiteBuilder.build());
+        return "redirect:/login";
     }
 }
