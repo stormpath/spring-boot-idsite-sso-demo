cbbcc482f42f3c648e168debdb6814c9c349a84e
Micah Silverman <micah@afitnerd.com>
2015-10-03 11:44:17 -0400

    Removed unnecessary code from HomeController.

--- src/main/java/com/stormpath/idsite_demo/controllers/HomeController.java
+++ src/main/java/com/stormpath/idsite_demo/controllers/HomeController.java
@@ -2,7 +2,6 @@ package com.stormpath.idsite_demo.controllers;
 
 import com.stormpath.sdk.application.Application;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +11,8 @@ public class HomeController {
     @Autowired
     Application app;
 
-    @Value("#{ @environment['stormpath.template.root'] }")
-    private String templateRoot;
-
-
     @RequestMapping("/")
     public String home(Model model) {
-        model.addAttribute("appName", app.getName());
-        model.addAttribute("appDescription", app.getDescription());
-
         return "index";
     }
 }
