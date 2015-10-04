c56bf6b8d416e7eb8b8a9af8d6e9458e3f9e3f8e
Micah Silverman <micah@afitnerd.com>
2015-10-03 23:29:33 -0400

    added debug to show headers

--- src/main/java/com/stormpath/idsite_demo/controllers/HomeController.java
+++ src/main/java/com/stormpath/idsite_demo/controllers/HomeController.java
@@ -3,7 +3,13 @@ package com.stormpath.idsite_demo.controllers;
 import com.stormpath.sdk.application.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -13,5 +19,18 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         return "index";
+    }
+
+    @RequestMapping("/debug")
+    public String debug(HttpServletRequest req, Model model) {
+        model.addAttribute("request", req);
+        Map<String, String> headers = new HashMap<>();
+        Enumeration<String> names = req.getHeaderNames();
+        while (names.hasMoreElements()) {
+            String name = names.nextElement();
+            headers.put(name, req.getHeader(name));
+        }
+        model.addAttribute("headers", headers);
+        return "debug";
     }
 }
