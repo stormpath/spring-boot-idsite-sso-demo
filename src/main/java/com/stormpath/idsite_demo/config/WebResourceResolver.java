d196e6453642028e931f8a8f955c83c2ff0d5425
Micah Silverman <micah@afitnerd.com>
2016-07-07 18:09:35 -0400

    Updated with latest version. Got rid of unnecessary local repo. Updated WebResourceResolver and RestrictedController to use ix as default.

--- src/main/java/com/stormpath/idsite_demo/config/WebResourceResolver.java
+++ src/main/java/com/stormpath/idsite_demo/config/WebResourceResolver.java
@@ -1,24 +1,16 @@
 package com.stormpath.idsite_demo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.view.ThymeleafView;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
-import org.thymeleaf.templateresolver.TemplateResolver;
-
-import javax.annotation.PostConstruct;
 
 @Configuration
 public class WebResourceResolver extends WebMvcConfigurerAdapter {
-    @Value("#{ @environment['stormpath.template.root'] }")
-    private String templateRoot = "ix";
+    @Value("#{ @environment['stormpath.template.root'] ?: 'ix' }")
+    private String templateRoot;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/com/stormpath/idsite_demo/controllers/RestrictedController.java
+++ src/main/java/com/stormpath/idsite_demo/controllers/RestrictedController.java
@@ -1,43 +1,35 @@
 package com.stormpath.idsite_demo.controllers;
 
 import com.stormpath.sdk.account.Account;
-import com.stormpath.sdk.lang.Strings;
 import com.stormpath.sdk.servlet.account.AccountResolver;
-import org.springframework.beans.factory.annotation.Value;
+import com.stormpath.sdk.servlet.http.Resolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RestrictedController {
-    @Value("#{ @environment['stormpath.organization'] }")
-    private String organization;
 
-    @Value("#{ @environment['stormpath.show.organization.field'] }")
-    private Boolean showOrganizationField;
-
-    @Value("#{ @environment['stormpath.template.root'] }")
-    private String templateRoot = "ro";
+    @Autowired
+    Resolver<String> stormpathOrganizationNameKeyResolver;
 
     @RequestMapping("/restricted/user")
-    public String restricted(HttpServletRequest req, Model model) {
+    public String restricted(HttpServletRequest req) {
         Account account = AccountResolver.INSTANCE.getAccount(req);
-        if (account != null) {
-            String url = req.getRequestURL().toString();
-            Boolean showCrossLink = (Strings.countOccurrencesOf(url, ".") > 1) ? false : true;
-            model.addAttribute("showCrossLink", showCrossLink);
 
-            String template = "restricted/user";
-            if (url.contains("sith")) {
-                template = "restricted/sith";
-            } else if (url.contains("stormtrooper")) {
-                template = "restricted/stormtrooper";
-            }
-            return template;
+        if (account != null) {
+            String org = getOrganizationNameKey(req);
+            String templateSuffix = (org == null) ? "user" : org;
+
+            return "restricted/" + templateSuffix;
         }
 
         return "redirect:/login";
+    }
+
+    private String getOrganizationNameKey(HttpServletRequest req) {
+        return stormpathOrganizationNameKeyResolver.get(req, null);
     }
 }
